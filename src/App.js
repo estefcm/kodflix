feb33fe2aecd8cd4d89d0be2a29f732525a25767
Estefania cardona <estefania@MBPdeEstefania.localdomain>
2019-12-02 16:34:34 +0000

    Redirect to a "not-found" page when the targeted show doesn't exist #15

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Gallery from './Gallery';
 import Details from './Details';
 import './App.css';
+import NotFound from './NotFound';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Route  exact path = '/' component={Gallery}/>
-          <Route  exact path = '/:TvshowId' component={Details}/>
+          <Switch>
+            <Route exact path='/' component={Gallery} />
+            <Route exact path='/not-found' component={NotFound} />
+            <Route exact path='/:TvshowId' component={Details} />
+          </Switch>
         </div>
       </Router>
     );
--- src/Details.js
+++ src/Details.js
@@ -1,31 +1,35 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import getGallery from "./gallery-get.js";
 
- 
 
-export default  class Details extends Component {
+
+export default class Details extends Component {
 
   constructor() {
     super();
     this.state = {
-     Tvshow: {}
+      Tvshow: {}
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     let TvshowId = this.props.match.params.TvshowId;
     let Tvshow = getGallery().find((Tvshow) => Tvshow.id === TvshowId);
-   
-    this.setState({Tvshow});
+
+    this.setState({ Tvshow });
   }
-   
+
   render() {
-    return(
-      <div>
-          <h1>{this.state.Tvshow.name}</h1>
-          <Link to='/'>Back to home page</Link>
-      </div>
-    );
+    if (this.state.Tvshow === undefined) {
+      return <Redirect to='/not-found' />;
+    } else {
+        return (
+          <div>
+             <h1>{this.state.Tvshow.name}</h1>
+             <Link to='/'>Back to home page</Link>
+          </div>
+        );
+      }
+    }
   }
-}
