8440762c5768a4e9c997007b59e3d939c2e7db74
Estefania cardona <estefania@MBPdeEstefania.localdomain>
2019-12-09 11:22:28 +0000

    Return a list of shows from the backend #19

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const getGallery = require('./shows');
+const port = 3001;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/rest/shows', (req, res) => {
+  res.send(getGallery());
 });
+app.get('/' ,(req, res) => res.send('HOLA'))
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
