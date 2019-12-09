6d39e27e4e582f4be19968a91d49ec254602b42e
Estefania cardona <estefania@MBPdeEstefania.localdomain>
2019-12-09 11:31:06 +0000

    Improve setup of backend server #20

--- src/backend/app.js
+++ src/backend/app.js
@@ -6,7 +6,7 @@ const port = 3001;
 app.get('/rest/shows', (req, res) => {
   res.send(getGallery());
 });
-app.get('/' ,(req, res) => res.send('HOLA'))
+// app.get('/' ,(req, res) => res.send('HOLA'))
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
